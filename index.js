ab6a51e2dd6022cf5f22b7f5d8b8dc8c3eab4af7
Aung Kaung Myat <105598393+knightterris@users.noreply.github.com>
2024-01-09 11:01:39 +0630

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function auth(req, res, next) {
 
 function makeSecurityKey(length) {
   let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789*@#$_';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._';
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
